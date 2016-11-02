4015a971fabfd441626cf418584a5f19014af90a
rahmanwr <rahmanwr@gmail.com>
2016-11-02 09:01:00 -0400

    initial commit

---
+++ src/CalculateCircle.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;        
+
+public class CalculateCircle {
+   
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	 double radius = 0.0;
+    	System.out.println(" Welcome to Circle Tester" ); 
+        System.out.print("Enter radius: " );
+        radius = scanner.nextInt();
+        double area = 0.0;
+      
+      area = Math.PI * radius * radius;
+      double circumference= Math.PI * 2*radius;
+ 
+      System.out.println("Radius  " + radius);
+      System.out.println("Area of circle:"+area);
+      System.out.println( "The circumference of the circle is:" +circumference);
+    }  
+}
+
